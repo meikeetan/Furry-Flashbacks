5ccb0500a4d524b64bbb8d090f130b8f82b84d3e
meikeetan <152507735+meikeetan@users.noreply.github.com>
2024-02-03 02:43:44 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@ function resetGame() {
 
 //fetchData function
 function fetchData(catTypes) {
-  fetch(`https://meikeetan.github.io/Furry-Flashbacks/data/latte.json`) 
+  fetch(`https://meikeetan.github.io/Furry-Flashbacks/data/${catTypes}.json`) 
  .then((res) => res.json())
     .then((data) => {
       cards = [...data, ...data]; 

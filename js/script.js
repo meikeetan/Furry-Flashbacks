3b7f302315d92f402ab932274068eba5d6e09201
meikeetan <152507735+meikeetan@users.noreply.github.com>
2024-02-03 02:24:46 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,10 +75,18 @@ function resetGame() {
 //fetchData function
 function fetchData(catTypes) {
   fetch(`../data/${catTypes}.json`) 
-    .then((res) => res.json())
+   .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Network response was not ok: ${res.statusText}`);
+      }
+      return res.json();
+    })
     .then((data) => {
-      cards = [...data, ...data]; 
+      cards = [...data, ...data];
       resetGame();
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
     });
 }
 
